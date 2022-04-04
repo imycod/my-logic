7479a3c8aa3a7d14f5aac508de87256356dd90d3
imycod <317009160@qq.com>
2022-04-04 23:54:44 +0800

    增加节流函数常用于滚动

--- src/utils.js
+++ src/utils.js
@@ -39,6 +39,24 @@ const debounce = (fn, wait, immediate = false) => {
   }
 }
 
+const throttle = function (func, delay) {
+  var timer = null;
+  var startTime = Date.now();
+  return function () {
+    var curTime = Date.now();
+    var remaining = delay - (curTime - startTime);
+    var context = this;
+    var args = arguments;
+    clearTimeout(timer);
+    if (remaining <= 0) {
+      func.apply(context, args);
+      startTime = Date.now();
+    } else {
+      timer = setTimeout(func, remaining);
+    }
+  }
+}
+
 export function importModule(moduleName, module, ...params) {
   const paramArray = params ? params.concat(importModule) : [importModule]
   console.log(this instanceof builder);
